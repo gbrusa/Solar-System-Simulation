1fe8462b4d840787cdc055359b6753b6b4929db8
Giulio Brusa <gbrusa@cs.washington.edu>
2018-04-16 23:48:19 -0700

    Added stuff

---
+++ World Simulator/Assets/Scripts/DisplayMoney.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayMoney : MonoBehaviour {
+
+	public Text moneyDisplay;
+
+	public static float money;
+
+	// Use this for initialization
+	void Start () {
+		money = 0;
+		moneyDisplay.text = "Money: $" + money;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moneyDisplay.text = "Money: $" + money;
+	}
+}
---
+++ World Simulator/Assets/Scripts/GameFunction.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameFunction : MonoBehaviour {
+
+	public float collectSpeed = 1;
+	public float salary;
+	public float price;
+	public bool locked;
+	public Material[] materials;
+	public Text priceTag;
+
+	private float nextCollect = 0.0f;
+	private float myTime = 0.0f;
+	private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+		nextCollect = collectSpeed;
+		if (locked) {
+			rend.sharedMaterial = materials [1];
+			GetComponent<Rotator>().enabled = false;
+			priceTag.text = "$" + price;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (locked) {
+			if (Input.GetMouseButtonDown (0) && DisplayMoney.money - price >= 0) {
+				locked = false;
+				rend.sharedMaterial = materials [0];
+				GetComponent<Rotator> ().enabled = true;
+				priceTag.enabled = false;
+			}
+		} else {
+			myTime += Time.deltaTime;
+			if (myTime >= nextCollect) {
+				nextCollect = myTime + collectSpeed;
+				DisplayMoney.money += salary;
+				nextCollect -= myTime;
+				myTime = 0.0f;
+			}
+		}
+	}
+}
