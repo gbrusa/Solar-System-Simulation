3869b873b952c48a40dde8ed45b482d93a851974
Leo Brusa <giulio.r.brusa@gmail.com>
2019-03-04 22:35:34 -0800

    Add files via upload

---
+++ Assets/RingRotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingRotator : MonoBehaviour {
+
+	public float speed;
+
+	//private float parentSpeed;
+
+	void Start() {
+		//parentSpeed = this.transform.parent.GetComponent<Rotator> ().speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0, 0, 1) * speed); // + new Vector3 (0, 0, -1) * speed);
+	}
+}
